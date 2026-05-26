9d157c42db79574bab5a48bc1a4a67c8ccb0a697
Rand8805 <56931739+Rand8805@users.noreply.github.com>
2026-05-25 20:27:41 -0500

    Add files via upload

--- videoteca.py
+++ videoteca.py
@@ -1,8 +1,13 @@
+# Andres Fernando Henao Parra
+# 213022_878
+# Ingenieria Electronica 
+# Código Fuente: autoría propia
 titulos = ["Todo Poderoso", "El Padrino", "Forrest Gump", "El Origen", "Matrix", "El Señor de los Anillos", "Gladiador", "El Club de la Pelea", "El Gran Gatsby", "El Rey León", "Titanic", "Avatar", "Jurassic Park", "El Resplandor", "El Silencio de los Corderos"]
 años = [2003, 1972, 1994, 2010, 1999, 2001, 2000, 1999, 2013, 1994, 1997, 2009, 1993, 1980, 1991]
 calificaciones = [6.8, 9.2, 8.8, 8.8, 8.7, 8.8, 8.5, 8.8, 7.2, 8.5, 7.8, 7.8, 8.1, 8.4, 8.6]
 generos = ["Comedia", "Crimen", "Drama", "Ciencia Ficción", "Acción", "Aventura", "Épica", "Thriller", "Romance", "Animación", "Romance", "Ciencia Ficción", "Aventura", "Terror", "Thriller"]
 videoteca = [titulos, años, calificaciones, generos]
+contador = 0
 print("Bienvenido a la videoteca. Aquí tienes las películas disponibles según calificación y año:")
 for i in range(len(videoteca[0])):
     if i==0:
@@ -30,3 +35,6 @@ print("Películas con calificación cercana a", calificacion, "y año cercano a"
 for i in range(len(videoteca[0])):
     if (videoteca[2][i] >= (calificacion - 1.5) and videoteca[2][i] <= (calificacion + 1.5)) and (videoteca[1][i] >= (año)):
         print(videoteca[0][i], "-", videoteca[1][i], "-", videoteca[2][i], "-", videoteca[3][i])
+        contador += 1
+if contador == 0:
+    print("No se encontraron películas que cumplan con los criterios.")
